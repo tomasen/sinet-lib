a04156bcc38472a17da231052a6005d29217ecc1
dym <dym@localhost>
2010-09-19 16:00:52 +0800

    1???post buffer?crash 2???define?????? 3?????proxy?????

---
+++ trunk/sinet/strings.h
@@ -0,0 +1,17 @@
+#ifndef STRINGS_H
+#define STRINGS_H
+
+namespace sinet
+{
+
+
+class strings
+{
+public:
+  static std::string wstring_utf8string(const std::wstring& s);
+};
+
+
+} // namespace sinet
+
+#endif // STRINGS_H
