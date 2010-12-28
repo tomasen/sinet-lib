ecb345b1bdd14f1b8ba834977fddbee25a886095
Tomasen <Tomasen@localhost>
2010-12-28 14:19:43 +0800

    fix strings operation

--- trunk/sinet/strings.cc
+++ trunk/sinet/strings.cc
@@ -6,21 +6,27 @@ using namespace sinet;
 // Convert a std::wstring to a std::string (utf8)
 std::string strings::wstring_utf8string(const std::wstring& s)
 {
+#ifdef WIN32 
   char* ch;
   UINT bytes = WideCharToMultiByte(CP_UTF8, 0, s.c_str(), -1, NULL, 0,
-    NULL, NULL); 
+                                   NULL, NULL); 
   ch = new char[bytes];
   if(ch)
     bytes = WideCharToMultiByte(CP_UTF8, 0, s.c_str(), -1, ch, bytes,
-    NULL, NULL);
+                                NULL, NULL);
   std::string str = ch;
   delete[] ch;
   return str;
+#endif
+#ifdef _MAC_
+  return std::string((const char*)Utf8(s.c_str()));
+#endif
 }
 
 // Convert a std::string to a std::wstring (utf8)
 std::wstring strings::utf8string_wstring(const std::string& s)
 {
+#ifdef WIN32  
   wchar_t* wch;
   UINT bytes = MultiByteToWideChar(CP_UTF8, 0, s.c_str(), -1, NULL, 0);
   wch  = new wchar_t[bytes];
@@ -29,4 +35,8 @@ std::wstring strings::utf8string_wstring(const std::string& s)
   std::wstring str = wch;
   delete[] wch;
   return str;
+#endif
+#ifdef _MAC_
+  return std::wstring((const wchar_t*)Utf8(s.c_str()));
+#endif
 }
